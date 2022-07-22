18563c1a3c14b587af7ba50dc3c290308a7ec978
DenisVoron <Denis.voronko@gmail.com>
2022-07-22 22:55:42 +0300

    to do task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+const NumberOfCategories = document.querySelectorAll('.item')
+
+console.log(NumberOfCategories);
+console.log('Number of categories:', NumberOfCategories.length);
+
+NumberOfCategories.forEach((number, index) => {
+    console.log(`Индекс ${index}, значение ${number}`);
+});
