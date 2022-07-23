5b06e6d39309752c4fcc5bfc3efdc1af78f54b57
DenisVoron <Denis.voronko@gmail.com>
2022-07-23 14:43:06 +0300

    process make task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainerEl = document.querySelector('#ingredients');
+//console.log(ingredientsContainerEl);
+/*const listEl = document.createElement('li');
+listEl.textContent = ingredients;
+listEl.classList = 'item';
+console.log(listEl);
+ingredientsContainerEl.prepend(listEl);*/
+
+const elements = ingredients.forEach(option => {
+  const listEl = document.createElement('li');
+  listEl.textContent = option;
+  listEl.classList = 'item';
+  //console.log(option);
+  console.log(listEl);
+  return listEl;
+});
+console.log(elements);
+
+//console.log(ingredients);
+
+/*const makingListOfIngredients = element => {
+  return element.forEach(option => {
+    const listEl = document.createElement('li');
+    listEl.textContent = option;
+    listEl.classList = 'item';
+    //console.log(option);
+    console.log(listEl);
+    return listEl;
+  });
+};
+const elements = makingListOfIngredients(ingredients);
+ingredientsContainerEl.append(...elements);
+/*ingredientsContainerEl.append();
+console.log(elements);*/
