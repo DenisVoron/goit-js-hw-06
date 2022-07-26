dba8354494f72d83036c500fc941dfd374f4088e
DenisVoron <Denis.voronko@gmail.com>
2022-07-26 12:34:18 +0300

    made task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.body;
+const entersColor = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onClickChangeColor);
+
+function onClickChangeColor() {
+  const color = getRandomHexColor();
+
+  body.style.backgroundColor = color;
+  entersColor.textContent = color;
+}
